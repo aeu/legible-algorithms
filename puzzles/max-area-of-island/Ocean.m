35244e8fb4bc2b504ca20cdc91048534c355b68a
Alfonso Urdaneta <alfonso@red82.com>
2021-12-17 23:04:07 -0500

    Max Area of Island - Objective C

---
+++ puzzles/max-area-of-island/Ocean.m
@@ -0,0 +1,201 @@
+// -*- Mode: objc; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//  alfonso e. urdaneta
+//  alfonso@red82.com
+//
+
+#import "Ocean.h"
+
+
+@implementation Ocean
+
+
+-(int)currentIslandId
+{
+    return current_island_id;
+}
+
+-(int)createIsland
+{
+    current_island_id++;
+    return [self currentIslandId];
+}
+
+-(OceanNode)nodeForLocation:(OceanLocation)targetLocation
+{
+    OceanNode retval;
+    retval.land         = -1;
+    retval.parentIsland = 0;
+    
+    if( targetLocation.row < 0 )
+        return retval;
+
+    if( targetLocation.row >= [ _grid count] )
+        return retval;
+
+    if( targetLocation.column < 0 )
+        return retval;
+    
+    NSMutableArray *columns = [_grid objectAtIndex: targetLocation.row ];
+
+    if( targetLocation.column >= [columns count] )
+        return retval;
+
+    NSValue *node_value = [columns objectAtIndex:targetLocation.column];
+    [node_value getValue:&retval];
+
+    return retval;
+}
+
+
+-(void)updateLocation:(OceanLocation)targetLocation
+             withNode:(OceanNode)newNode
+{
+    NSValue *node_value = [NSValue valueWithBytes:&newNode objCType:@encode(OceanNode)];
+    NSMutableArray *columns = [_grid objectAtIndex: targetLocation.row ];
+    [columns replaceObjectAtIndex:targetLocation.column withObject:node_value];
+}
+
+-(OceanLocation)getNorth:(OceanLocation)currentLocation
+{
+    OceanLocation destination;
+    destination.row     = currentLocation.row -1 ;
+    destination.column  = currentLocation.column ;
+    return destination;
+}
+
+-(OceanLocation)getSouth:(OceanLocation)currentLocation
+{
+    OceanLocation destination;
+    destination.row     = currentLocation.row + 1 ;
+    destination.column  = currentLocation.column ;
+    return destination;
+}
+
+-(OceanLocation)getEast:(OceanLocation)currentLocation
+{
+    OceanLocation destination;
+    destination.row     = currentLocation.row;
+    destination.column  = currentLocation.column + 1 ;
+    return destination;
+}
+
+-(OceanLocation)getWest:(OceanLocation)currentLocation
+{
+    OceanLocation destination;
+    destination.row     = currentLocation.row;
+    destination.column  = currentLocation.column - 1 ;
+    return destination;
+}
+
+
+-(int)surveyNode:(OceanLocation)node_location
+        islandId:(int)islandId
+{
+    int retval = 0;
+    OceanNode current_node = [self nodeForLocation:node_location];
+    // if its land
+    if( current_node.land == 1 )
+    {
+        // and doesn't have an island set
+        if( current_node.parentIsland == 0 )
+        {
+            // set it and count it
+            current_node.parentIsland = islandId;
+            [self updateLocation:node_location withNode:current_node];
+            retval++;
+
+            // then check the surrounding nodes
+            OceanLocation north = [self getNorth:node_location];
+            retval += [self surveyNode:north islandId:islandId];
+
+            OceanLocation south = [self getSouth:node_location];
+            retval += [self surveyNode:south islandId:islandId];
+            
+            OceanLocation east = [self getEast:node_location];
+            retval += [self surveyNode:east islandId:islandId];
+            
+            OceanLocation west = [self getWest:node_location];
+            retval += [self surveyNode:west islandId:islandId];
+        }
+    }
+    return retval;
+}
+
+-(void)processNodes
+{
+    int biggest = 0;
+    int size = 0;
+    for(int row_index = 0; row_index < [ _grid count]; row_index++ )
+    {
+        NSMutableArray *current_row = [_grid objectAtIndex:row_index];
+        for( int col_index = 0; col_index < [current_row count]; col_index++)
+        {
+            OceanNode current_node;
+            NSValue *node_value = [current_row objectAtIndex:col_index];
+            [node_value getValue:&current_node];
+            if( current_node.land )
+            {
+                OceanLocation node_location;
+                node_location.row    = row_index;
+                node_location.column = col_index;
+                int island_id = [self createIsland];
+                size = [self surveyNode:node_location islandId:island_id];
+
+                if( size > biggest )
+                    biggest = size;
+            }
+        }
+    }
+    printf("The biggest island was %d nodes\n", biggest);
+}
+
+
+
+
+-(Ocean *)initWithValues:(int *)values numRows:(int)numRows numColumns:(int)numColumns
+{
+    if( self = [super init] )
+    {
+        current_island_id = 1000;
+        NSMutableArray *rows = [[NSMutableArray alloc] init];
+        for( int row_index = 0; row_index < numRows; row_index++ )
+        {
+            NSMutableArray *current_row = [[NSMutableArray alloc] init];
+            for( int col_index = 0; col_index < numColumns; col_index++ )
+            {
+                int current_index = ( row_index * numColumns ) + col_index;
+                OceanNode current_node;
+                current_node.land = values[ current_index ];
+                current_node.parentIsland = 0;
+                //
+                // we can't put a c struct into a obj-c container class, so make a NSValue
+                //
+                NSValue *node_value = [NSValue valueWithBytes:&current_node objCType:@encode(OceanNode)];
+                [current_row addObject:node_value];
+            }
+            [rows addObject:current_row];
+        }
+        [self setGrid:rows];
+    }
+    return self;
+}
+
+-(void)dumpGrid
+{
+    for(int row_index = 0; row_index < [ _grid count]; row_index++ )
+    {
+        NSMutableArray *current_row = [_grid objectAtIndex:row_index];
+        for( int col_index = 0; col_index < [current_row count]; col_index++)
+        {
+            OceanNode current_node;
+            NSValue *node_value = [current_row objectAtIndex:col_index];
+            [node_value getValue:&current_node];
+            printf("%d ", current_node.land );
+        }
+        printf("\n");
+    }
+}
+
+@end
---
+++ puzzles/max-area-of-island/README.md
@@ -0,0 +1,31 @@
+# Max Area of Island.
+
+Source : leetcode.
+
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+    0010000100000
+    0000000111000
+    0110100000000
+    0100110010100
+    0100110011100
+    0000000000100
+    0000000111000
+    0000000110000
+
+
+Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+
+Output: 6
+
+Explanation: The answer is not 11, because the island must be connected 4-directionally.
+
+
+Solution:
+I liked this problem because it allowed me to use my favourite GoF
+pattern, Composite, recursion, which is also cool, and a Depth First
+traversal.
