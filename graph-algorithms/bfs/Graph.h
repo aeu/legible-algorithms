d37b9b1871d98e7979f36ca9b589ffbba314794f
Alfonso Urdaneta <alfonso@red82.com>
2022-02-22 13:21:03 -0500

    progress checkin

---
+++ graph-algorithms/bfs/Graph.h
@@ -0,0 +1,19 @@
+// -*- Mode: objc; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//  alfonso e. urdaneta
+//  alfonso@red82.com
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+
+@interface Graph : NSObject
+{
+    
+}
+
+@end
+
+#endif
---
+++ graph-algorithms/bfs/Graph.m
@@ -0,0 +1,13 @@
+// -*- Mode: objc; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//  alfonso e. urdaneta
+//  alfonso@red82.com
+//
+#import <Foundation/Foundation.h>
+#import "Graph.h"
+
+
+@implementation Graph
+
+@end
