6fa036db349af67d59c3537a04a1980ad3e9863e
Alfonso Urdaneta <alfonso@red82.com>
2022-02-22 13:21:03 -0500

    progress checkin

---
+++ graph-algorithms/bfs/bfs.m
@@ -0,0 +1,13 @@
+// -*- Mode: objc; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//  alfonso e. urdaneta
+//  alfonso@red82.com
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("BFS!\n");
+}
