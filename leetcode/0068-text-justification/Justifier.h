b1142f77abfd38ea00235512e2fba983ab325ebc
Alfonso Urdaneta <alfonso.urdaneta@carrier.com>
2023-11-02 16:47:01 -0400

    Code cleanup

--- leetcode/0068-text-justification/Justifier.h
+++ leetcode/0068-text-justification/Justifier.h
@@ -10,17 +10,18 @@
 class Justifier {
  public:
 
-    int dumpWordList(int start_position, int end_position );
-    void dumpWords(int start_point, int justification_width );
-    int dumpJustifiedLine(int start_position, int end_position, int justification_width );
-    int minimumLengthForRange(int start_position, int end_position);
-    int wordLengthOnlyForRange(int start_position, int end_position);
     Justifier(std::vector<std::string> words);
+
+    void finalLine(int start_point, int justification_width );
+    void justifiedLine(int start_position, int end_position, int justification_width );
     void justify(int width);
     
  protected:
- private:
-    void debug();
+
+    int minimumLengthForRange(int start_position, int end_position);
+    int wordLengthOnlyForRange(int start_position, int end_position);
+
+private:
 
     std::vector<std::string> words;
 
