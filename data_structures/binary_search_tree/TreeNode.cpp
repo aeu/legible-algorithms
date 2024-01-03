101398febe15f7478ce4f275f59ed273aad77710
Alfonso Urdaneta <alfonso@red82.com>
2024-01-03 10:01:59 -0500

    snapshot progress checkin

---
+++ data_structures/binary_search_tree/TreeNode.cpp
@@ -0,0 +1,8 @@
+#include "TreeNode.h"
+
+TreeNode::TreeNode()
+{
+    left = nullptr;
+    right = nullptr;
+    value = 0;
+}
---
+++ data_structures/binary_search_tree/TreeNode.h
@@ -0,0 +1,14 @@
+#ifndef TREE_NODE
+#define TREE_NODE
+
+class TreeNode {
+ public:
+    TreeNode();
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+ protected:
+ private:
+};
+
+#endif
