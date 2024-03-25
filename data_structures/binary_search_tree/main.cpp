b7154e18c97adb94197fcd62fb91e460f664fc30
Alfonso Urdaneta <alfonso@red82.com>
2024-03-25 18:45:38 -0400

    improve the output of the debug code

--- data_structures/binary_search_tree/main.cpp
+++ data_structures/binary_search_tree/main.cpp
@@ -7,149 +7,145 @@ int main(int argc, char **argv)
     BinarySearchTree main_tree;
     int new_value;
 
+    printf("\n--- populating tree ---\n");
     new_value = 15;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 22;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 17;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 2;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 5;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 45;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 12;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 6;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 4;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 100;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 120;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 88;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 67;
-    printf("adding value %d\n", new_value);
-    main_tree.addValue( new_value );
-
-    new_value = 45;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 54;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 32;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 29;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 33;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 97;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 44;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
     new_value = 86;
-    printf("adding value %d\n", new_value);
+    printf("\tadding value %d\n", new_value);
     main_tree.addValue( new_value );
 
-    printf("--- inOrderTreeWalk ---\n");
+    printf("\n--- inOrderTreeWalk ---\n");
     BinarySearchTree::inOrderTreeWalk( main_tree.root );
     printf("\n");
 
     int target_value;
     TreeNode *target_node;
 
-    printf("--- treeSearch ---\n");
+    printf("\n--- treeSearch ---\n");
 
     target_value = 17;
     target_node = BinarySearchTree::treeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 13;
     target_node = BinarySearchTree::treeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 15;
     target_node = BinarySearchTree::treeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 120;
     target_node = BinarySearchTree::treeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 122;
     target_node = BinarySearchTree::treeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
 
-    printf("--- iterativeTreeSearch ---\n");
+    printf("\n--- iterativeTreeSearch ---\n");
 
     target_value = 17;
     target_node = BinarySearchTree::iterativeTreeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 13;
     target_node = BinarySearchTree::iterativeTreeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 15;
     target_node = BinarySearchTree::iterativeTreeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 120;
     target_node = BinarySearchTree::iterativeTreeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
     target_value = 122;
     target_node = BinarySearchTree::iterativeTreeSearch( main_tree.root, target_value );
-    printf("treeSearch for %d result %p\n", target_value, target_node );
+    printf("\ttreeSearch for %d result %p\n", target_value, target_node );
 
 
-    printf("--- treeMinimum ---\n");
+    printf("\n--- treeMinimum ---\n");
     TreeNode *minimum_node = BinarySearchTree::treeMinimum( main_tree.root );
-    printf("minimum node %p, value %d \n", minimum_node, minimum_node->value );
+    printf("\tminimum node %p, value %d \n", minimum_node, minimum_node->value );
 
-    printf("--- treeMaximum ---\n");
+    printf("\n--- treeMaximum ---\n");
     TreeNode *maximum_node = BinarySearchTree::treeMaximum( main_tree.root );
-    printf("maximum node %p, value %d \n", maximum_node, maximum_node->value );
-
+    printf("\tmaximum node %p, value %d \n", maximum_node, maximum_node->value );
 }
