56d39a1b6c5dd6802d105169f67907f21c7a66b9
Alfonso Urdaneta <alfonso@red82.com>
2024-06-28 13:50:51 -0400

    stub project and makefile

---
+++ leetcode/0085-maximal-rectangle/main.cpp
@@ -0,0 +1,21 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <stdio.h>
+#include <vector>
+#include "extra.h"
+
+void doWork(std::vector<std::vector<char> > &data)
+{
+}
+
+int main(int argc, char **argv)
+{
+    printf("starting\n");
+    std::vector<int> ints = {10, 20, 30};
+    extra();
+}
