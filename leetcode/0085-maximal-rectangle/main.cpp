651d8834884cce9dd91341107121883c24923bcb
Alfonso Urdaneta <alfonso@red82.com>
2024-07-16 00:38:37 -0400

    progress checkin

--- leetcode/0085-maximal-rectangle/main.cpp
+++ leetcode/0085-maximal-rectangle/main.cpp
@@ -9,13 +9,50 @@
 #include <vector>
 #include "extra.h"
 
+int getHeightFromPosition(std::vector<std::vector<char> > &data, int row, int col)
+{
+    int height_at_position = 0;
+    auto num_rows = data.size();
+    std::vector<char> current_row;
+    for(auto row_index = row; row_index < num_rows; row_index++ )
+    {
+        current_row = data[row_index];
+        if( current_row[ col ] == '1' )
+            height_at_position++;
+        else
+            break;
+    }
+    return height_at_position;
+}
+
 void doWork(std::vector<std::vector<char> > &data)
 {
+    auto num_rows = data.size();
+    auto num_columns = data[0].size();
+    auto row_index = 0;
+    auto col_index = 0;
+    
+    printf("in do work, got data with %lu rows of %lu columns\n", num_rows, num_columns);
+    std::vector<char> current_row;
+    for(row_index=0; row_index< num_rows; row_index++)
+    {
+        current_row = data[row_index];
+        for(col_index = 0; col_index < num_columns; col_index++ )
+        {
+            int height_at_position = getHeightFromPosition( data, row_index, col_index);
+            printf("(%d,%d) = %c, height: %d\n", row_index,col_index,current_row[col_index], height_at_position);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char **argv)
 {
     printf("starting\n");
-    std::vector<std::vector<int> > ints = {{10, 20, 30}, {12, 25, 36}};;
-    extra();
+    std::vector<std::vector<char> > data =
+        {{'1','0','1','0','0'},
+         {'1','0','1','1','1'},
+         {'1','1','1','1','1'},
+         {'1','0','0','1','0'}};
+    doWork( data );
 }
