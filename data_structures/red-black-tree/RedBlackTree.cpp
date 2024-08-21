aa58bcb0664b7a3eaa272bb130b2fa592318a788
Alfonso Urdaneta <alfonso@red82.com>
2024-08-21 09:35:58 -0400

    incremental progress on Red Black Tree implementation

--- data_structures/red-black-tree/RedBlackTree.cpp
+++ data_structures/red-black-tree/RedBlackTree.cpp
@@ -39,18 +39,28 @@ std::shared_ptr<TreeNode> RedBlackTree::insert(std::shared_ptr<TreeNode> new_nod
 }
 
 
-std::shared_ptr<TreeNode> RedBlackTree::maximum()
+std::shared_ptr<TreeNode> RedBlackTree::maximum(std::shared_ptr<TreeNode> start_node)
 {
-    std::shared_ptr<TreeNode> maximum = root_node;
+    std::shared_ptr<TreeNode> maximum = start_node;
+    if( maximum == nullptr )
+        maximum = root_node;
     while( maximum != nullptr && maximum->getRight() != nullptr )
     {
         maximum = maximum->getRight();
     }
     return maximum;
 }
-std::shared_ptr<TreeNode> RedBlackTree::minimum()
+
+std::shared_ptr<TreeNode> RedBlackTree::minimum(std::shared_ptr<TreeNode> start_node)
 {
-    return nullptr;
+    std::shared_ptr<TreeNode> minimum = start_node;
+    if( minimum == nullptr )
+        minimum = root_node;
+    while( minimum != nullptr && minimum->getLeft() != nullptr )
+    {
+        minimum = minimum->getLeft();
+    }
+    return minimum;
 }
 
 
@@ -60,3 +70,39 @@ std::shared_ptr<TreeNode> RedBlackTree::addValue(int new_value)
     insert(new_node);
     return new_node;
 }
+
+
+std::shared_ptr<TreeNode> RedBlackTree::successor(std::shared_ptr<TreeNode> start_node)
+{
+    if( start_node->getRight() != nullptr )
+        return minimum(start_node->getRight());
+
+    std::shared_ptr<TreeNode> temp_node = start_node;
+    std::shared_ptr<TreeNode> successor = start_node->getParent();
+
+    while( successor != nullptr && temp_node == successor->getRight() )
+    {
+        temp_node = successor;
+        successor = successor->getParent();
+    }
+    return successor;
+}
+
+
+std::shared_ptr<TreeNode> RedBlackTree::predecessor(std::shared_ptr<TreeNode> start_node)
+{
+    if( start_node->getLeft() != nullptr )
+        return maximum(start_node->getLeft());
+
+    std::shared_ptr<TreeNode> predecessor = start_node->getParent();
+    std::shared_ptr<TreeNode> temp_node = start_node;
+
+    while( predecessor != nullptr && temp_node == predecessor->getLeft() )
+    {
+        temp_node = predecessor;
+        predecessor = predecessor->getParent();
+    }
+    return predecessor;
+}
+
+
--- data_structures/red-black-tree/rbt.cpp
+++ data_structures/red-black-tree/rbt.cpp
@@ -6,20 +6,87 @@
 int main(int argc, char **argv)
 {
     RedBlackTree temp2;
-    temp2.addValue(15);
-    temp2.addValue(6);
-    //    temp2.addValue(5);
-    temp2.addValue(3);
-    temp2.addValue(7);
-    temp2.addValue(10);
-    temp2.addValue(12);
-    temp2.addValue(13);
-    temp2.addValue(16);
-    temp2.addValue(20);
-    temp2.addValue(18);
-    temp2.addValue(23);
+    std::shared_ptr<TreeNode> fifteen = temp2.addValue(15);
+
+    temp2.addValue( 22 );
+    std::shared_ptr<TreeNode> seventeen = temp2.addValue( 17 );
+    temp2.addValue( 2 );
+    temp2.addValue( 5 );
+    temp2.addValue( 45 );
+    temp2.addValue( 12 );
+    temp2.addValue( 6 );
+    temp2.addValue( 4 );
+    temp2.addValue( 100 );
+    temp2.addValue( 120 );
+    temp2.addValue( 88 );
+    temp2.addValue( 67 );
+    std::shared_ptr<TreeNode> fifty_four = temp2.addValue( 54 );
+    temp2.addValue( 32 );
+    temp2.addValue( 29 );
+    temp2.addValue( 33 );
+    temp2.addValue( 97 );
+    temp2.addValue( 44 );
+    temp2.addValue( 86 );
+    
+    // std::shared_ptr<TreeNode> seven = temp2.addValue(7);
+    // temp2.addValue(10);
+    // temp2.addValue(12);
+    // temp2.addValue(13);
+    // temp2.addValue(47);
+    // std::shared_ptr<TreeNode> sixteen = temp2.addValue(16);
+    // temp2.addValue(20);
+    // temp2.addValue(18);
+    // temp2.addValue(23);
 
 
-    std::shared_ptr<TreeNode> maximum = temp2.maximum(nullptr);
-    printf("the max node was : %d \n", maximum->getValue());
+    // {
+    //     std::shared_ptr<TreeNode> maximum = temp2.maximum(nullptr);
+    //     printf("the max node was : %d \n", maximum->getValue());
+    // }
+
+    // {
+    //     std::shared_ptr<TreeNode> minimum = temp2.minimum(nullptr);
+    //     printf("the min node was : %d \n", minimum->getValue());
+    // }
+
+    // {
+    //     std::shared_ptr<TreeNode> maximum = temp2.maximum(sixteen);
+    //     printf("the max node under 16 was : %d \n", maximum->getValue());
+    // }
+
+    // {
+    //     std::shared_ptr<TreeNode> minimum = temp2.minimum(seven);
+    //     printf("the min node under 7 was : %d \n", minimum->getValue());
+    // }
+
+
+    {
+        std::shared_ptr<TreeNode> successor = temp2.successor(fifty_four);
+        printf("the successor of 54 was : %d \n", successor->getValue());
+    }
+
+
+    {
+        std::shared_ptr<TreeNode> predecessor = temp2.predecessor(fifty_four);
+        printf("the predecessor of 54 was : %d \n", predecessor->getValue());
+    }
+
+
+
+    {
+        std::shared_ptr<TreeNode> successor = temp2.successor(seventeen);
+        printf("the successor of 17 was : %d \n", successor->getValue());
+    }
+
+
+    {
+        std::shared_ptr<TreeNode> predecessor = temp2.predecessor(seventeen);
+        printf("the predecessor of 17 was : %d \n", predecessor->getValue());
+    }
+
+
+
+
+
+    
 }
