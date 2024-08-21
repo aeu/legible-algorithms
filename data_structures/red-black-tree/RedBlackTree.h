dda1716ef734b018aa5123e95c8748886f18caba
Alfonso Urdaneta <alfonso.urdaneta@carrier.com>
2024-08-21 09:35:58 -0400

    incremental progress on Red Black Tree implementation

--- data_structures/red-black-tree/RedBlackTree.h
+++ data_structures/red-black-tree/RedBlackTree.h
@@ -9,16 +9,18 @@
 class RedBlackTree {
     
  public:
+
     RedBlackTree();
     
     std::shared_ptr<TreeNode> addValue(int new_value);
-    std::shared_ptr<TreeNode> maximum();
-    std::shared_ptr<TreeNode> minimum();
+    std::shared_ptr<TreeNode> maximum(std::shared_ptr<TreeNode> start_node);
+    std::shared_ptr<TreeNode> minimum(std::shared_ptr<TreeNode> start_node);
+    std::shared_ptr<TreeNode> successor(std::shared_ptr<TreeNode> start_node);
+    std::shared_ptr<TreeNode> predecessor(std::shared_ptr<TreeNode> start_node);
     
  protected:
 
     std::shared_ptr<TreeNode> insert(std::shared_ptr<TreeNode> new_node);
-    
 
 private:
 
