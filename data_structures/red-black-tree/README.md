3ed28a588a886e052a3341e59c55a743483b0663
Alfonso Urdaneta <alfonso@red82.com>
2024-12-27 08:08:05 -0500

    Progress checkin of Red Black Tree

--- data_structures/red-black-tree/README.md
+++ data_structures/red-black-tree/README.md
@@ -1,7 +1,12 @@
-Every node in a red black tree has the following properties
+## Red Black Tree
 
-Left
-Right
-Parent
-Colour
+## Introduction
+
+Red Black trees are the data structure that we all kind of take for granted.  Conceptually we know what they are (a type of binary search tree), what they are good for (where it is important to optimize a tree to maintain consistent operation execution times) and where they are use (std::map is typically implemented as a RBT.
+
+We (or maybe it's just me) see Red Black Tree in texts and go to ourselves "I know what those are" and keep going.  But it's not until you actually type it up that you actually understand how it works and what it is doing
+
+## Implementation
+
+In addition to being a lot more typing than I anticipated, impementing an actual working tree also required understanding of what was going on.  Most of the documentation and implementations I have seen online are all seem to be based on Introduction to Algorithms (CLRS) which is actually an imcomplete implementation, so most of the blogs and code sites that I've seen this implemented on only have half of it done.
 
--- data_structures/red-black-tree/rbt.cpp
+++ data_structures/red-black-tree/rbt.cpp
@@ -76,7 +76,6 @@ int main(int argc, char **argv)
     sprintf(filename,"%03d.dot",file_index++);
     temp2.toDotFile( filename, temp2.nilNode());
 
-#if 0
 
     temp2.addValue(45);
     sprintf(filename,"%03d.dot",file_index++);
@@ -129,37 +128,7 @@ int main(int argc, char **argv)
 
     temp2.inOrderTreeWalk(temp2.root_node);
 
-
-
-
-    
-    temp2.addValue( 22  );
-    std::shared_ptr<TreeNode> seventeen = temp2.addValue(17);
-    temp2.addValue( 2   );
-    std::shared_ptr<TreeNode> five = temp2.addValue(5);
-    temp2.addValue( 45  );
-    temp2.addValue( 12  );
-    temp2.addValue( 6   );
-    temp2.addValue( 4   );
-    temp2.addValue( 100 );
-    temp2.addValue( 120 );
-    std::shared_ptr<TreeNode> eighty_eight = temp2.addValue(88);
-    temp2.addValue( 67  );
-    std::shared_ptr<TreeNode> fifty_four = temp2.addValue(54);
-    temp2.addValue( 32  );
-    temp2.addValue( 29  );
-    temp2.addValue( 33  );
-    temp2.addValue( 97  );
-    temp2.addValue( 44  );
-    temp2.addValue( 86  );
-    std::shared_ptr<TreeNode> seven = temp2.addValue(7);
-    temp2.addValue(10);
-    temp2.addValue(13);
-    temp2.addValue(47);
-    std::shared_ptr<TreeNode> sixteen = temp2.addValue(16);
-    temp2.addValue(20);
-    temp2.addValue(18);
-    temp2.addValue(23);
+#if 0
 
     {
         std::shared_ptr<TreeNode> maximum = temp2.maximum( temp2.nilNode());
