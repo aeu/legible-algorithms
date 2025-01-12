a543c61c10ddc57fa01d78e7264ccd92e1974f54
Alfonso Urdaneta <alfonso@red82.com>
2025-01-12 00:05:06 -0500

    0283 move zeroes initial

---
+++ leetcode/0283-move-zeroes/zeroes.cpp
@@ -0,0 +1,62 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
+void dumpNumbers(std::vector<int> numbers)
+{
+    bool first = true;
+    for(int current : numbers )
+    {
+        if( ! first )
+        {
+            std::cout << ", ";
+        }
+        std::cout << current ;
+        first = false;
+    }
+    std::cout << std::endl;
+}
+
+void moveZeroes(std::vector<int> &numbers)
+{
+    int last = numbers.size() - 1;
+    for(int index=0;index<last;index++)
+    {
+        if( numbers[index] == 0 )
+        {
+            for(int current=index;current<last;current++)
+            {
+                numbers[current] = numbers[current+1];
+            }
+            numbers[last] = 0;
+            last--;
+        }
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    std::cout << "Leetcode #283 - Move Zeroes" << std::endl << std::endl;
+    {
+        std::vector<int> numbers = { 0,1,0,3,12 };
+        dumpNumbers( numbers );
+        moveZeroes(numbers);
+        dumpNumbers( numbers );
+    }
+    {
+        std::vector<int> numbers = { 0 };
+        dumpNumbers( numbers );
+        moveZeroes(numbers);
+        dumpNumbers( numbers );
+    }
+}
