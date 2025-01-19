08e048b56110412a4a9f1bc4566d8492909b32a5
Alfonso Urdaneta <alfonso@red82.com>
2025-01-19 11:38:51 -0500

    LC2130 - max twin sum of linked list

---
+++ leetcode/2130-maximum-twin-sum-of-a-linked-list/ListNode.cpp
@@ -0,0 +1,66 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include "ListNode.h"
+
+ListNode::ListNode()
+{
+    value = 0;
+    next = nullptr;
+}
+ListNode::ListNode(const int node_value)
+{
+    value = node_value;
+    next = nullptr;
+}
+
+ListNode::ListNode(const int node_value,
+                   std::shared_ptr<ListNode> next_value)
+{
+    value = node_value;
+    next = next_value;
+}
+
+std::shared_ptr<ListNode> ListNode::getNext()
+{
+    return next;
+}
+std::shared_ptr<ListNode> ListNode::setNext(std::shared_ptr<ListNode> next_value)
+{
+    next = next_value;
+    return next;
+}
+
+int ListNode::getValue()
+{
+    return value;
+}
+
+int ListNode::setValue(const int new_value)
+{
+    value = new_value;
+    return value;
+}
+
+
+void ListNode::dumpNodes()
+{
+    bool first_time = true;
+    std::cout << "[";
+    std::shared_ptr<ListNode> next_node = shared_from_this();
+    
+    while (next_node != nullptr)
+    {
+        if (!first_time)
+            std::cout << ", ";
+
+        std::cout << next_node->getValue();
+        next_node = next_node->getNext();
+        first_time = false;
+    }
+    std::cout << "]" << std::endl;
+}
