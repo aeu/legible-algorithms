da6ba3e905793d9ef0bdbbc6744bf7fa48679463
Alfonso Urdaneta <alfonso@red82.com>
2025-01-23 19:15:20 -0500

    basic int TreeNode

---
+++ misc/binary-tree/TreeNode.cpp
@@ -0,0 +1,59 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include "TreeNode.h"
+
+TreeNode::TreeNode()
+{
+    setValue(0);
+    setLeft(nullptr);
+    setRight(nullptr);
+}
+
+TreeNode::TreeNode(const int new_value)
+{
+    setValue(new_value);
+    setLeft(nullptr);
+    setRight(nullptr);
+}
+
+TreeNode::TreeNode(const int new_value,
+                   std::shared_ptr<TreeNode> new_left,
+                   std::shared_ptr<TreeNode> new_right)
+{
+    setValue(new_value);
+    setLeft(new_left);
+    setRight(new_right);
+}
+
+
+std::shared_ptr<TreeNode> TreeNode::getLeft()
+{
+    return nullptr;
+}
+
+std::shared_ptr<TreeNode> getRight()
+{
+    return nullptr;
+}
+
+std::shared_ptr<TreeNode> TreeNode::setLeft(std::shared_ptr<TreeNode> new_left)
+{
+    left = new_left;
+    return left;
+}
+ 
+std::shared_ptr<TreeNode> TreeNode::setRight(std::shared_ptr<TreeNode> new_right)
+{
+    right = new_right;
+    return right;
+}
+
+void TreeNode::setValue(const int new_value)
+{
+    value = new_value;
+}
---
+++ misc/binary-tree/btt.cpp
@@ -0,0 +1,13 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  return -1;
+}
