12ee0cee75f8c8600a0688916ada59d18641d0d7
Alfonso Urdaneta <alfonso@red82.com>
2025-01-25 18:26:16 -0500

    cleanup of tree nodes for LC104

--- leetcode/0104-maximum-depth-of-binary-tree/mdb.cpp
+++ leetcode/0104-maximum-depth-of-binary-tree/mdb.cpp
@@ -43,52 +43,6 @@ void dumpNode(std::shared_ptr<TreeNode> current_node )
     }
 }
 
-void dumpTree(std::shared_ptr<TreeNode> current_node )
-{
-    std::cout << "[";
-    dumpNode(current_node);
-    std::cout << "]" << std::endl;
-}
-
-
-std::shared_ptr<TreeNode> buildTree(const std::vector<std::optional<int>> &values)
-{
-    if( values.empty() )
-        return nullptr;
-
-    std::shared_ptr<TreeNode> root_node;
-    std::queue<std::shared_ptr<TreeNode>> node_queue;
-    if( values[0].has_value() )
-    {
-        root_node = std::make_shared<TreeNode>(values[0].value());
-        node_queue.push( root_node );
-    }
-
-    int index = 1;
-    while( index < values.size () )
-    {
-        std::shared_ptr<TreeNode> current = node_queue.front();
-        node_queue.pop();
-
-        if( values[index].has_value() )
-        {
-            std::shared_ptr<TreeNode> left_node = std::make_shared<TreeNode>(values[index].value());
-            current->setLeft(left_node);
-            node_queue.push(left_node);
-        }
-        index++;
-        if((index < values.size()) && ( values[index].has_value() ))
-        {
-            std::shared_ptr<TreeNode> right_node = std::make_shared<TreeNode>(values[index].value());
-            current->setRight(right_node);
-            node_queue.push(right_node);
-        }
-        index++;
-    }
-    return root_node;
-}
-
-
 int maxDepth(std::shared_ptr<TreeNode> current)
 {
     int left_depth = 0;
@@ -109,7 +63,7 @@ int main(int argc, char **argv)
         std::vector<std::optional<int>> values = { 3,9,20,std::nullopt,std::nullopt,15,7 };
         std::cout << "Input : " ;
         dumpValues( values );
-        std::shared_ptr<TreeNode> root = buildTree(values);
+        std::shared_ptr<TreeNode> root = TreeNode::buildTree(values);
         int max_depth = maxDepth( root );
         std::cout << "Max depth : " << max_depth << std::endl;
     }
@@ -117,7 +71,7 @@ int main(int argc, char **argv)
         std::vector<std::optional<int>> values = { 1,std::nullopt,2 };
         std::cout << "Input : " ;
         dumpValues( values );
-        std::shared_ptr<TreeNode> root = buildTree(values);
+        std::shared_ptr<TreeNode> root = TreeNode::buildTree(values);
         int max_depth = maxDepth( root );
         std::cout << "Max depth : " << max_depth << std::endl;
     }
