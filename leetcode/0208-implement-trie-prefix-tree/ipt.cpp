8479cdf17dbd9a2990367ec6710fdcb3ad33efd6
Alfonso Urdaneta <alfonso@red82.com>
2025-02-12 06:44:31 -0500

    progress checkin of the Trie

---
+++ leetcode/0208-implement-trie-prefix-tree/ipt.cpp
@@ -0,0 +1,18 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <memory>
+#include "Trie.h"
+
+
+int main(int argc, char **argv)
+{
+    Trie trie;
+    trie.insert("nachos");
+    return 0;
+}
