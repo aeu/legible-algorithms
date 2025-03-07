37fb954b858cd2408b9f0a0321c16f04b6cb6792
Alfonso Urdaneta <alfonso@red82.com>
2025-03-07 10:04:23 -0500

    progress checkin on rotary lock c2.  runs too slow

--- meta/rotary-lock-chapter-2/rlc.cpp
+++ meta/rotary-lock-chapter-2/rlc.cpp
@@ -1,3 +1,9 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
 #include <vector>
 #include <iostream>
 #include <unordered_map>
@@ -6,9 +12,40 @@
 #include <set>
 
 using namespace std;
-using namespace std;
 // Write any include statements here
 
+struct CombinationStage
+{
+    int step;
+    int left_lock;
+    int right_lock;
+
+    bool operator<(const CombinationStage &rhs) const
+    {
+        if (step != rhs.step)
+            return step < rhs.step;
+        if (left_lock != rhs.left_lock)
+            return left_lock < rhs.left_lock;
+        return right_lock < rhs.right_lock;
+    }
+
+    bool operator==(const CombinationStage &rhs) const
+    {
+        return (( step == rhs.step ) && ( left_lock == rhs.left_lock ) && ( right_lock == rhs.right_lock ));
+    }
+
+    
+};
+
+struct CombinationStageHash
+{
+    size_t operator()(const CombinationStage &s) const
+    {
+        return std::hash<int>()(s.step) ^ (std::hash<int>()(s.left_lock) << 1 ) ^ (std::hash<int>()(s.right_lock) << 2);
+    }
+};
+
+std::unordered_map<CombinationStage,long long, CombinationStageHash> dpmemo;
 
 long distanceTo(int current, int destination, int N)
 {
@@ -32,29 +69,34 @@ long distanceTo(int current, int destination, int N)
 }
 
 
-long long getMinCodeEntryTime(int N, int M, vector<int> C) {
+long long dpSearch(int step, int left_lock, int right_lock, vector<int> &C, int N)
+{
+    if( step == C.size() )
+        return 0;
+    
+    CombinationStage current;
+    current.step = step;
+    current.left_lock = left_lock;
+    current.right_lock = right_lock;
+    
+    if( dpmemo.find( current ) != dpmemo.end() )
+        return dpmemo[current];
 
-    int left_current = 1;
-    int right_current = 1;
-    long long total_time = 0;
-    for(auto current : C )
-    {
-        int left_time  = distanceTo(left_current,current,N);
-        int right_time = distanceTo(right_current,current,N);
-        if( left_time <= right_time )
-        {
-            left_current = current;
-            total_time += left_time;
-        }
-        else
-        {
-            right_current = current;
-            total_time += right_time;
-        }
-    }
-    return total_time;
+    int target = C[step];
+    // distance to get the left lock to target, plus the distance after the lft lock is at target.
+    long long left_distance = distanceTo( left_lock, target, N ) + dpSearch( step+1, target, right_lock, C, N );
+
+    // distance to get the right lock to target, plus the distance after the right lock is at target.
+    long long right_distance = distanceTo( right_lock, target, N ) + dpSearch( step+1, left_lock, target, C, N );
+
+    return dpmemo[current] = std::min( left_distance, right_distance );
 }
 
+long long getMinCodeEntryTime(int N, int M, vector<int> &C)
+{
+    dpmemo.clear();
+    return dpSearch(0,1,1,C,N);
+}
 
 int main(int argc, char **argv)
 {
@@ -82,4 +124,5 @@ int main(int argc, char **argv)
             std::cout << "fail got " << mincode << " should be " << expected << std::endl;
            
     }
+    return 0;
 }
