1c912c3d031d0d664a7b8cbd69452b46f64edb57
Alfonso Urdaneta <alfonso.urdaneta@honeywell.com>
2025-04-02 15:03:12 -0400

    90 incremental

---
+++ leetcode/0090-subsets-ii/sii.cpp
@@ -0,0 +1,63 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+
+void backtrack(std::vector<int> &nums,
+               int start_index,
+               std::vector<int> &path,
+               std::vector<std::vector<int>> &retval)
+{
+    for(int index=start_index,index<nums.size();index++)
+    {
+        if( nums[index] == nums[index-1] )
+            continue;
+        path.push_back(nums[index]);
+        backtrack(nums,start_index+1,path,retval);
+        path.pop_back();
+    }
+}
+
+std::vector<std::vector<int>> subsetsWithDup(std::vector<int> &nums)
+{
+    std::vector<std::vector<int>> retval;
+    std::vector<int> path;
+    std::sort(nums.begin(),nums.end());
+    backtrack(nums,0,path,retval);
+    return retval;
+}
+
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "LC90 - Subsets II" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<int> nums  = {1,2,2};
+        std::vector<std::vector<int>> expected = {{},{1},{1,2},{1,2,2},{2},{2,2}};
+        std::vector<std::vector<int>> result = subsetsWithDup(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+    }
+    {
+        std::vector<int> nums  = {0};
+        std::vector<std::vector<int>> expected = {{},{0}};
+        std::vector<std::vector<int>> result = subsetsWithDup(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+    }
+    return 0;
+}
