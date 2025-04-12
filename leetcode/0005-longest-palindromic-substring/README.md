97e9e7264ac6d81b528684a77ff59e5fd896b845
Alfonso Urdaneta <alfonso@red82.com>
2025-04-12 15:15:10 -0400

    LC0005 longest palindromic substring

---
+++ leetcode/0005-longest-palindromic-substring/README.md
@@ -0,0 +1,37 @@
+
+LC0005 - Longest Palindromic Substring
+
+nimlen : 0  start point : 0
+nimlen : 2  start point : 1
+nimlen : 0  start point : 2
+
+Test case : 1 : Pass
+ (expected bb, got bb)
+nimlen : 2  start point : 1
+nimlen : 0  start point : 2
+
+Test case : 2 : Pass
+ (expected ccc, got ccc)
+nimlen : 0  start point : 0
+nimlen : 0  start point : 1
+nimlen : 0  start point : 2
+nimlen : 0  start point : 3
+nimlen : 0  start point : 4
+
+Test case : 3 : Pass
+ (expected bab, got bab)
+nimlen : 0  start point : 0
+nimlen : 2  start point : 1
+nimlen : 0  start point : 2
+nimlen : 0  start point : 3
+
+Test case : 4 : Pass
+ (expected bb, got bb)
+
+Test case : 5 : Pass
+ (expected a, got a)
+nimlen : 0  start point : 0
+nimlen : 0  start point : 1
+
+Test case : 6 : Pass
+ (expected a, got a)
---
+++ leetcode/0005-longest-palindromic-substring/lps.cpp
@@ -0,0 +1,165 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+std::pair<int,std::string> palindromeAtStartIndex(std::string s,
+                                             int start_index )
+{
+    std::string retval = "";
+    if( s.length() == 0 )
+        return { -1, "" };
+
+    // the worse case is that the very first char is the palindrome so make that the default return.
+    int max_length = 1;
+    retval += s[0];
+
+    if( s.length() == 1 )
+        return { max_length, retval };
+
+    if( start_index == 0 )
+    {
+        // longest possible pali at start position 0 is 2.
+        if( s.length() >= 2 )
+        {
+            if( s[0] == s[1] )
+                return { 2, s.substr(0,2) };
+        }
+    }
+
+    // palindrome can either have an isolated middle char or not.  Check first for isolated middle.
+    int left = start_index-1;
+    int right = start_index+1;
+    int pairs = 0;
+    while(( left >= 0 ) && ( right < s.length() ))
+    {
+        if( s[left] == s[right] )
+        {
+            pairs++;
+            left--;
+            right++;
+        }
+        else
+            break;
+    }
+    if( pairs > 0 )
+    {
+        int length = 1 + ( pairs * 2 );
+        int start = start_index - pairs;
+        if( length > max_length )
+        {
+            max_length = length;
+            retval = s.substr(start,length);
+        }
+    }
+
+    // now check for the non-isolated middle
+    left = start_index;
+    right = start_index+1;
+    int start_point = start_index;
+    int nimlen = 0;
+    while(( left >= 0 ) && ( right < s.length() ))
+    {
+        if( s[left] == s[right] )
+        {
+            start_point = left;
+            nimlen+=2;
+            left--;
+            right++;
+        }
+        else
+        {
+            break;
+        }
+    }    
+    std::cout << "nimlen : " << nimlen << "  start point : " << start_point << std::endl;
+    if( nimlen > max_length )
+    {
+        max_length = nimlen;
+        retval = s.substr(start_point,nimlen );
+    }
+    return { max_length, retval };
+}
+
+std::string longestPalindrome(std::string s)
+{
+    std::string longest = "";
+    int max_length = INT_MIN;
+    for(int index=0;index<s.length();index++)
+    {
+        std::pair<int,std::string> curr = palindromeAtStartIndex(s,index);
+        if( curr.first > max_length )
+        {
+            max_length = curr.first;
+            longest = curr.second;
+        }
+    }
+    return longest;
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "LC0005 - Longest Palindromic Substring" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::string s = "abb";
+        std::string expected = "bb";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::string s = "ccc";
+        std::string expected = "ccc";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::string s = "babad";
+        std::string expected = "bab";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::string s = "cbbd";
+        std::string expected = "bb";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::string s = "a";
+        std::string expected = "a";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::string s = "ac";
+        std::string expected = "a";
+        std::string result = longestPalindrome(s);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    return 0;
+}
