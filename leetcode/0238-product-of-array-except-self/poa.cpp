f4aa11c76d8e4354bd82dca93f535fb1cd6d6c78
Alfonso Urdaneta <alfonso@red82.com>
2025-04-12 10:16:49 -0400

    LC238 - product of array except self

--- leetcode/0238-product-of-array-except-self/poa.cpp
+++ leetcode/0238-product-of-array-except-self/poa.cpp
@@ -4,61 +4,76 @@
 //
 //  This software may not be used or reproduced, in whole or in part,
 //  without the express written permission of red82
-#include <stdio.h>
-#include <vector>
-#include <string>
+
 #include <iostream>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
 
-void dumpNumbers(const std::vector<int> &numbers)
+void dumpValues(std::vector<int> vals)
 {
-    bool first_time = true;
-    std::cout << "[";
-    for(int current : numbers )
+    for( auto curr : vals )
     {
-        if( ! first_time )
-            std::cout << ", ";
-        std::cout << current ;
-        first_time = false;
+        std::cout << curr << ", " ;
     }
-    std::cout << "]" << std::endl;
+    std::cout << std::endl;
 }
+
+std::vector<int> productExceptSelf(std::vector<int>& nums)
+{
+    std::vector<int> retval     = std::vector<int>( nums.size(),0 );
+    std::vector<int> left_pass  = std::vector<int>( nums.size(),0 );
+    std::vector<int> right_pass = std::vector<int>( nums.size(),0 );
+
+    left_pass[0] = 1;
     
-std::vector<int> productOfArrayExceptSelf(const std::vector<int> &numbers)
-{
-    std::vector<int> results ( numbers.size(),1);
-
-    for(int index=1;index<numbers.size();index++)
+    for(int index = 1; index < nums.size();index++)
     {
-        results[index] = numbers[index-1] * results[index-1];
+        left_pass[index] = nums[index-1] * left_pass[index-1];
     }
 
-    int right_product = 1;
-    for(int index=numbers.size()-2;index>=0;index--)
+    right_pass[ nums.size()-1] = 1;
+
+    for(int index=nums.size()-2;index>=0;index--)
     {
-        right_product *= numbers[index+1];
-        results[index] *= right_product;
+        right_pass[index] = nums[index+1] * right_pass[index+1];
     }
-    return results;
+
+    for(int index = 0; index < nums.size();index++)
+    {
+        if( index == 0 )
+            retval[index] = right_pass[index];
+        else if ( index == nums.size() -1 )
+            retval[index] = left_pass[index];
+        else
+            retval[index] = left_pass[index] * right_pass[index];
+    }
+    return retval;
 }
+
 
 int main(int argc, char **argv)
 {
-    std::cout << "Leetcode #238 - Product of Array Except Self" << std::endl;
+    std::cout << std::endl << "238 Product of Array Except Self" << std::endl << std::endl;
+    int test_case = 1;
     {
-        std::vector<int> numbers = { 5,2,3,4};
-        std::cout << "Input  : ";
-        dumpNumbers( numbers );
-        std::vector<int> results = productOfArrayExceptSelf(numbers);
-        std::cout << "Results: ";
-        dumpNumbers(results);
+        std::vector<int> nums  = {1,2,3,4};
+        std::vector<int> expected = { 24,12,8,6 };
+        std::vector<int> result = productExceptSelf( nums );
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
     }
     {
-        std::vector<int> numbers = { -1,1,0,-3,3};
-        std::cout << "Input  : ";
-        dumpNumbers( numbers );
-        std::vector<int> results = productOfArrayExceptSelf(numbers);
-        std::cout << "Results: ";
-        dumpNumbers(results);
+        std::vector<int> nums  = {-1,1,0,-3,3};
+        std::vector<int> expected = {0,0,9,0,0};
+        std::vector<int> result = productExceptSelf( nums );
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
     }
-    return 1;
+    return 0;
 }
