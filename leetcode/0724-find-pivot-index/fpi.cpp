00968e0c9e46be35a813fb3cef5b0e3ec05a90c4
Alfonso Urdaneta <alfonso@red82.com>
2025-04-18 09:15:11 -0400

    LC724 - find pivot index, improve

--- leetcode/0724-find-pivot-index/fpi.cpp
+++ leetcode/0724-find-pivot-index/fpi.cpp
@@ -4,72 +4,62 @@
 //
 //  This software may not be used or reproduced, in whole or in part,
 //  without the express written permission of red82
-//
-#include <stdio.h>
-#include <limits.h>
-#include <string>
-#include <cmath>
-#include <vector>
-#include <algorithm>
+
 #include <iostream>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
 
-void dumpNumbers(const std::vector<int> &numbers )
+int pivotIndex(std::vector<int>& nums)
 {
-    bool first_time = true;
-    for(int current : numbers )
+    int allsum = 0;
+    for( auto curr : nums )
     {
-        if( ! first_time )
-            std::cout << ", " ;
-        std::cout << current ;
-        first_time = false;
+        allsum += curr;
     }
-    std::cout << std::endl;
-}   
-
-
-int findPivotIndex(const std::vector<int> &numbers)
-{
-    if( numbers.size() == 0 )
-        return -1;
-    int total = 0;
-    for( int current : numbers )
+    int strictly_left = 0;
+    for(int index=0;index<nums.size();index++)
     {
-        total += current;
-    }
-    int left_side = 0;
-    int right_side = total;
-    for(size_t index=0;index<numbers.size();index++)
-    {
-        right_side -= numbers[index];
-        if( left_side == right_side )
-        {
+        int strictly_right = allsum - strictly_left - nums[index];
+        if( strictly_left == strictly_right )
             return index;
-        }
-        left_side+=numbers[index];
+        strictly_left += nums[index];
     }
     return -1;
 }
 
-
 int main(int argc, char **argv)
 {
-    std::cout << "Leetcode 0724 - Find Pivot Index" << std::endl;
+    std::cout << std::endl << "0724-find-pivot-index" << std::endl << std::endl;
+    int test_case = 1;
     {
-        std::vector<int> numbers = {1,7,3,6,5,6};
-        int pivot_index = findPivotIndex(numbers);
-        dumpNumbers( numbers);
-        std::cout << "Pivot Index: " << pivot_index << std::endl;
+        std::vector<int> nums  = {1,7,3,6,5,6};
+        int expected = 3;
+        int result = pivotIndex(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
     }
     {
-        std::vector<int> numbers = {1,2,3};
-        int pivot_index = findPivotIndex(numbers);
-        dumpNumbers( numbers);
-        std::cout << "Pivot Index: " << pivot_index << std::endl;
+        std::vector<int> nums  = {1,2,3};
+        int expected = -1;
+        int result = pivotIndex(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
     }
     {
-        std::vector<int> numbers = {2,1,-1};
-        int pivot_index = findPivotIndex(numbers);
-        dumpNumbers( numbers);
-        std::cout << "Pivot Index: " << pivot_index << std::endl;
+        std::vector<int> nums  = {2,1,-1};
+        int expected = 0;
+        int result = pivotIndex(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
     }
+    return 0;
 }
