ba47d8407c5b80bc7f290f160d6b0aae64b9d571
Alfonso Urdaneta <alfonso@red82.com>
2025-04-27 11:10:29 -0400

    21 merge two sorted lists

---
+++ leetcode/0021-merge-two-sorted-lists/README.md
@@ -0,0 +1,8 @@
+
+0021-merge-two-sorted-lists
+
+1->2->4
+1->3->4
+1->1->2->3->4->4
+
+Test case : 1 : Pass
--- leetcode/0021-merge-two-sorted-lists/mts.cpp
+++ leetcode/0021-merge-two-sorted-lists/mts.cpp
@@ -4,83 +4,115 @@
 //
 //  This software may not be used or reproduced, in whole or in part,
 //  without the express written permission of red82
-#include <stdio.h>
-#include <vector>
+
 #include <iostream>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
 
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+ 
+struct Higher {
+    bool operator()(const ListNode *lhs, const ListNode *rhs)
+    {
+        return lhs->val > rhs->val;
+    }
 };
+
 
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
 {
-    if( list1 == nullptr )
-        return list2;
-    if( list2 == nullptr )
-        return list1;
-    
-    
-    ListNode *head;
-    ListNode *left = list1;
-    ListNode *right = list2;
-    ListNode *current_node;
-    ListNode *tail;
+    std::priority_queue<ListNode *, std::vector<ListNode *>, Higher> pqueue;
 
-    if( left->val <= right->val )
+    if( list1 != nullptr )
+        pqueue.push( list1 );
+    if( list2 != nullptr )
+        pqueue.push( list2 );
+    ListNode *newhead = nullptr ;
+    ListNode *newtail = nullptr ;
+    while( ! pqueue.empty() )
     {
-        head = left;
-        tail = head;
-        left = left->next;
-    }
-    else
-    {
-        head = right;
-        tail = head;
-        right = right->next;
-    }
-    
-    while( ( left != nullptr ) && ( right != nullptr ))
-    {
-        if( left->val <= right->val )
+        ListNode *curr = pqueue.top();
+        pqueue.pop();
+        ListNode *saved = curr->next;
+        if( newhead == nullptr )
         {
-            tail->next = left;
-            current_node = left;
-            left = left->next;
+            newhead = curr;
+            newtail = curr;
         }
-        else
+        else 
         {
-            tail->next = right;
-            current_node = right;
-            right = right->next;
+            newtail->next = curr;
+            newtail = curr;
         }
-        tail = current_node; 
+        if( saved != nullptr )
+            pqueue.push(saved);
     }
-
-    while( left != nullptr )
-    {
-        tail->next = left;
-        tail = left;
-        left = left->next;
-    }
-    while( right != nullptr )
-    {
-        tail->next = right;
-        tail = right;
-        right = right->next;
-    }
+    return newhead;
     
-    return head;
 }
 
-int main( int argc, char **argv)
+void dumpList(ListNode *head)
 {
-    printf("Leetcode #0021 - Merge Two Sorted Lists\n");
+    bool first = true;
+    ListNode *curr = head;
+    while( curr != nullptr )
     {
+        if( ! first )
+            std::cout << "->";
+        first = false;
+        std::cout << curr->val ;
+        curr = curr->next;
+    }
+    std::cout << std::endl;
+}
+
+
+    
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "0021-merge-two-sorted-lists" << std::endl << std::endl;
+    int test_case = 1;
+    {
+
+        ListNode *one = new ListNode(1);
+        ListNode *two = new ListNode(2);
+        ListNode *four = new ListNode(4);
+        
+        ListNode *oone = new ListNode(1);
+        ListNode *othree = new ListNode(3);
+        ListNode *ofour = new ListNode(4);
+
+        one->next = two;
+        two->next = four;
+
+        oone->next = othree;
+        othree->next = ofour;
+
+        dumpList(one);
+        dumpList(oone);
+        
+        ListNode *merged = mergeTwoLists(one,oone);
+        dumpList(merged);
+        
+        
+        int expected = 0;
+        int result = 0;
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
     }
     return 0;
 }
