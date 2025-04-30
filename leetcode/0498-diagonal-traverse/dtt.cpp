df7f1d83f148f86335defcccf3e28811109c5f70
Alfonso Urdaneta <alfonso@red82.com>
2025-04-30 11:52:56 -0400

    incremental

--- leetcode/0498-diagonal-traverse/dtt.cpp
+++ leetcode/0498-diagonal-traverse/dtt.cpp
@@ -9,6 +9,7 @@
 #include <optional>
 #include <vector>
 #include <queue>
+#include <algorithm>
 #include <map>
 #include <unordered_set>
 #include <unordered_map>
@@ -39,7 +40,7 @@ std::vector<int> findDiagonalOrder(std::vector<std::vector<int>>& mat)
         }
     }
     std::vector<int> retval;
-    int flipper = 0;
+    int flipper = 1;
     for(const auto citer : storage )
     {
         std::vector<int> nums = citer.second;
@@ -51,6 +52,7 @@ std::vector<int> findDiagonalOrder(std::vector<std::vector<int>>& mat)
         {
             retval.push_back(curr);
         }
+        flipper++;
     }
     return retval;
 }
@@ -60,6 +62,15 @@ int main(int argc, char **argv)
 {
     std::cout << std::endl << "0498-diagonal-traverse" << std::endl << std::endl;
     int test_case = 1;
+    {
+        std::vector<std::vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
+        std::vector<int> expected  = {1,2,4,7,5,3,6,8,9};
+        std::vector<int> result = findDiagonalOrder(mat);
+        dumpValues(result);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+    }
+    return 0;
     {
         std::vector<std::vector<int>> mat = {{1,2,3},{4,5,6},{7,8,9}};
         std::vector<int> expected  = {1,2,4,7,5,3,6,8,9};
