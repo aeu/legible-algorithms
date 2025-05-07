97ca37290efc7a8cb621d1d06c52238f5f1e31bf
Alfonso Urdaneta <alfonso@red82.com>
2025-05-07 00:19:30 -0400

    progress checkin

---
+++ leetcode/0270-closest-binary-search-tree-value/cbs.cpp
@@ -0,0 +1,33 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <iomanip>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "0270-closest-binary-search-tree-value" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<std::nullopt<int>> nums = {4,2,5,1,3};
+        float target = 3.713
+        int expected = 0;
+        int result = 0;
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    return 0;
+}
