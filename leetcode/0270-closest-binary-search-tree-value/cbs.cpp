08f1cfe53d5a76eb4321314b6a5655005b29e9c2
Alfonso Urdaneta <alfonso@red82.com>
2025-05-08 14:21:47 -0400

    progress

--- leetcode/0270-closest-binary-search-tree-value/cbs.cpp
+++ leetcode/0270-closest-binary-search-tree-value/cbs.cpp
@@ -16,6 +16,45 @@
 #include <stack>
 #include <limits.h>
 
+
+struct TreeNode {
+    int val;
+    TreeNode  *left;
+    TreeNode  *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *buildTree(std::vector<std::optional<int>> tree_values)
+{
+    TreeNode *root = new TreeNode(tree_values[0].value());
+    std::queue<TreeNode *>bqueue;
+    int index=1;
+    bqueue.push(root);
+    while( index < tree_values.size() )
+    {
+        TreeNode *curr = bqueue.front();
+        bqueue.pop();
+        if( tree_values[index].has_value() )
+        {
+            TreeNode *left = new TreeNode( tree_values[index].value() );
+            curr->left = left;
+            bqueue.push(left);
+        }
+        index++;
+        if( ( index < tree_values.size() ) && ( tree_values[index].has_value() ))
+        {
+            TreeNode *right = new TreeNode( tree_values[index].value() );
+            curr->right = right;
+            bqueue.push(right);
+        }
+        index++;
+    }
+    return root;
+}
+
+
 int main(int argc, char **argv)
 {
     std::cout << std::endl << "0270-closest-binary-search-tree-value" << std::endl << std::endl;
