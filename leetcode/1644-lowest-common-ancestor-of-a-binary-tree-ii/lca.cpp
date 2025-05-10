b2c7dc4b1ae2dfe110a224cf8fa1b49247703571
Alfonso Urdaneta <alfonso@red82.com>
2025-05-10 16:19:07 -0400

    progress checkin

---
+++ leetcode/1644-lowest-common-ancestor-of-a-binary-tree-ii/lca.cpp
@@ -0,0 +1,32 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <iomanip>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<int> nums  = {1,2,3};
+        int expected = 0;
+        int result = 0;
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    return 0;
+}
