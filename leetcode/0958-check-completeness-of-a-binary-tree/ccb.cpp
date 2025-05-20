1083dcc5d282e9d0208b43a240a532037ff40c42
Alfonso Urdaneta <alfonso@red82.com>
2025-05-19 21:29:43 -0400

    progress

---
+++ leetcode/0958-check-completeness-of-a-binary-tree/ccb.cpp
@@ -0,0 +1,128 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <iomanip>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+struct TreeNode {
+    int val;
+    TreeNode  *left;
+    TreeNode  *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void dumpNodeList(std::vector<TreeNode *> values)
+{
+    bool first = true;
+    for(const auto &curr : values )
+    {
+        if( ! first )
+            std::cout << "->" ;
+        first = false;
+        std::cout << curr->val;
+    }
+    std::cout << std::endl;
+}
+
+
+TreeNode *buildTree(std::vector<std::optional<int>> tree_values)
+{
+    TreeNode *root = new TreeNode(tree_values[0].value());
+    std::queue<TreeNode *>bqueue;
+    int index=1;
+    bqueue.push(root);
+    while( index < tree_values.size() )
+    {
+        TreeNode *curr = bqueue.front();
+        bqueue.pop();
+        if( tree_values[index].has_value() )
+        {
+            TreeNode *left = new TreeNode( tree_values[index].value() );
+            curr->left = left;
+            bqueue.push(left);
+        }
+        index++;
+        if( ( index < tree_values.size() ) && ( tree_values[index].has_value() ))
+        {
+            TreeNode *right = new TreeNode( tree_values[index].value() );
+            curr->right = right;
+            bqueue.push(right);
+        }
+        index++;
+    }
+    return root;
+}
+
+
+
+struct BfsInfo
+{
+    TreeNode *node;
+    int level;
+};
+
+bool isCompleteTree(TreeNode *root)
+{
+    if( root == nullptr )
+        return false;
+
+    std::queue<BfsInfo> bqueue;
+    int max_level = 0;
+    std::unordered_map<int,TreeNode *> last_nodes;
+    bqueue.push({root,0});
+    while( ! bqueue.empty() )
+    {
+        BfsInfo curr = bqueue.front();
+        bqueue.pop();
+        std::cout << "testing " << curr.node->val << std::endl;
+        last_nodes[curr.level] = curr.node;
+        max_level = std::max(max_level, curr.level );
+        if(( curr.node->left == nullptr ) && (curr.node->right != nullptr ))
+            return false;
+        if( curr.node->left != nullptr )
+            bqueue.push({ curr.node->left, curr.level+1 });
+        if( curr.node->right != nullptr )
+            bqueue.push({ curr.node->right, curr.level+1 });
+    }
+    return false;
+}
+
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "0958-check-completeness-of-a-binary-tree" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<std::optional<int>> values = {1,2,3,4,5,6};
+        TreeNode *root = buildTree(values);
+        bool expected = true;
+        bool result = isCompleteTree(root);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    {
+        std::vector<std::optional<int>> values = {1,2,3,4,std::nullopt,7};
+        TreeNode *root = buildTree(values);
+        bool expected = true;
+        bool result = isCompleteTree(root);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+    }
+    return 0;
+}
