da3a64d61dee7e67fcc4b17c6aab93719ff190ca
Alfonso Urdaneta <alfonso@red82.com>
2025-05-24 16:52:28 -0400

    LC700 - search in a binary search tree

--- leetcode/0700-search-in-a-binary-search-tree/sib.cpp
+++ leetcode/0700-search-in-a-binary-search-tree/sib.cpp
@@ -10,71 +10,72 @@
 #include <vector>
 #include <queue>
 #include <map>
-#include "TreeNode.h"
 
+struct TreeNode {
+    int val;
+    TreeNode  *left;
+    TreeNode  *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-void dumpValues(const std::vector<std::optional<int>> &values )
+void dumpNodeList(std::vector<TreeNode *> values)
 {
-    bool first_time = true;
-    std::cout << "[";
-    for( auto current : values )
+    bool first = true;
+    for(const auto &curr : values )
     {
-        if( ! first_time )
-            std::cout << ", ";
-        if( current.has_value() )
-            std::cout << current.value();
-        else
-            std::cout << "null";
-        first_time = false;
+        if( ! first )
+            std::cout << "->" ;
+        first = false;
+        std::cout << curr->val;
     }
-    std::cout << "]" << std::endl;
+    std::cout << std::endl;
 }
 
 
-std::shared_ptr<TreeNode> buildTree(const std::vector<std::optional<int>> &values )
+TreeNode *buildTree(std::vector<std::optional<int>> tree_values)
 {
-    if( values.empty() )
-        return nullptr;
-    size_t index = 0;
-    std::shared_ptr<TreeNode> root = std::make_shared<TreeNode>(values[index].value());
-    std::queue<std::shared_ptr<TreeNode>> nodes;
-    nodes.push(root);
-    index++;
-    while(index<values.size())
+    TreeNode *root = new TreeNode(tree_values[0].value());
+    std::queue<TreeNode *>bqueue;
+    int index=1;
+    bqueue.push(root);
+    while( index < tree_values.size() )
     {
-        std::shared_ptr<TreeNode> current = nodes.front();
-        nodes.pop();
-        if( values[index].has_value() )
+        TreeNode *curr = bqueue.front();
+        bqueue.pop();
+        if( tree_values[index].has_value() )
         {
-            std::shared_ptr<TreeNode> left = std::make_shared<TreeNode>( values[index].value());
-            current->setLeft(left);
-            nodes.push(left);
+            TreeNode *left = new TreeNode( tree_values[index].value() );
+            curr->left = left;
+            bqueue.push(left);
         }
         index++;
-        if((index<values.size()) && ( values[index].has_value() ))
+        if( ( index < tree_values.size() ) && ( tree_values[index].has_value() ))
         {
-            std::shared_ptr<TreeNode> right = std::make_shared<TreeNode>( values[index].value());
-            current->setRight(right);
-            nodes.push(right);
+            TreeNode *right = new TreeNode( tree_values[index].value() );
+            curr->right = right;
+            bqueue.push(right);
         }
         index++;
     }
     return root;
 }
 
-std::shared_ptr<TreeNode> searchBST(std::shared_ptr<TreeNode> root,
-                                    int val)
+
+TreeNode *searchBST(TreeNode *root,
+                    int val)
 {
     if( root == nullptr )
         return nullptr;
 
-    if( root->getValue() == val )
+    if( root->val == val )
         return root;
 
-    if( val < root->getValue() )
-        return searchBST( root->getLeft(),val);
+    if( val < root->val )
+        return searchBST( root->left,val);
     
-    return searchBST(root->getRight(),val);
+    return searchBST(root->right,val);
 }
 
 int main(int argc, char **argv)
@@ -84,21 +85,15 @@ int main(int argc, char **argv)
         int seek_value = 2;
         std::vector<std::optional<int>> values = { 4,2,7,1,3};
         std::cout << "Searching for : " << seek_value << " in : ";
-        dumpValues( values );
-        std::shared_ptr<TreeNode> root = buildTree(values);
-        std::shared_ptr<TreeNode> found_root = searchBST(root,seek_value);
-        std::cout << "Found Tree : ";
-        TreeNode::dumpTree(found_root);
+        TreeNode *root = buildTree(values);
+        TreeNode *found_root = searchBST(root,seek_value);
     }
     {
         int seek_value = 5;
         std::vector<std::optional<int>> values = { 4,2,7,1,3};
         std::cout << "Searching for : " << seek_value << " in : ";
-        dumpValues( values );
-        std::shared_ptr<TreeNode> root = TreeNode::buildTree(values);
-        std::shared_ptr<TreeNode> found_root = searchBST(root,seek_value);
-        std::cout << "Found Tree : ";
-        TreeNode::dumpTree(found_root);
+        TreeNode *root = root = buildTree(values);
+        TreeNode *found_root = searchBST(root,seek_value);
     }
     return -1;
 }
