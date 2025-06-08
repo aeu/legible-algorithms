ce4f684128824afcad4a2c0917636347148e0a74
Alfonso Urdaneta <alfonso@red82.com>
2025-06-07 23:42:38 -0400

    LC240 - search a 2d matrix ii

---
+++ leetcode/0240-search-a-2d-matrix-ii/README.md
@@ -0,0 +1,28 @@
+
+0240-search-a-2d-matrix-ii
+
+
+Test case : 1 : Pass
+ (expected 1, got 1)
+
+
+
+Test case : 2 : Pass
+ (expected 1, got 1)
+
+
+
+Test case : 3 : Pass
+ (expected 1, got 1)
+
+
+
+Test case : 4 : Pass
+ (expected 1, got 1)
+
+
+
+Test case : 5 : Pass
+ (expected 0, got 0)
+
+
---
+++ leetcode/0240-search-a-2d-matrix-ii/stm.cpp
@@ -0,0 +1,116 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <iomanip>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+void dumpValues(std::vector<int> values)
+{
+    bool first = true;
+    for(const auto &curr : values )
+    {
+        if( ! first )
+            std::cout << ", ";
+        first = false;
+        std::cout << std::setw(3) << curr ;
+    }
+    std::cout << std::endl;
+}
+
+
+bool searchMatrix(std::vector<std::vector<int>>& matrix, int target)
+{
+    int N = matrix.size();
+    int M = matrix[0].size();
+    int row = 0;
+    int col = M-1;
+
+    while(( row < N ) && ( col >= 0 ))
+    {
+        if( matrix[row][col] == target )
+            return true;
+        if( matrix[row][col] > target )
+        {
+            col--;
+        }
+        else if( matrix[row][col] < target )
+        {
+            row++;
+        }
+    }
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "0240-search-a-2d-matrix-ii" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<std::vector<int>> matrix = {{-1,3}};
+        int target = 3;
+        bool expected = true;
+        bool result = searchMatrix(matrix,target);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<std::vector<int>> matrix = {{-5}};
+        int target = -5;
+        bool expected = true;
+        bool result = searchMatrix(matrix,target);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<std::vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 5;
+        bool expected = true;
+        bool result = searchMatrix(matrix,target);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<std::vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 5;
+        bool expected = true;
+        bool result = searchMatrix(matrix,target);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<std::vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 20;
+        bool expected = false;
+        bool result = searchMatrix(matrix,target);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    return 0;
+}
