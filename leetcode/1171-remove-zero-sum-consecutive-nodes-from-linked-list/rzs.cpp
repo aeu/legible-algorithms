c3042561c2fd3dc5453ee74c1c2f6b8f2efeeeb0
Alfonso Urdaneta <alfonso@red82.com>
2025-06-14 15:47:33 -0400

    LC1171 - cleanup

--- leetcode/1171-remove-zero-sum-consecutive-nodes-from-linked-list/rzs.cpp
+++ leetcode/1171-remove-zero-sum-consecutive-nodes-from-linked-list/rzs.cpp
@@ -24,43 +24,78 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode *removeZeroSumSublists(ListNode *head)
+void dumpList(ListNode *list)
 {
-    return nullptr;
-}
-
-void dumpValues(std::vector<int> values)
-{
-    bool first = true;
-    for(const auto &curr : values )
+    ListNode *curr = list;
+    ListNode *original = list;
+    while(curr != nullptr )
     {
-        if( ! first )
-            std::cout << ", ";
-        first = false;
-        std::cout << std::setw(3) << curr ;
+        std::cout << curr->val;
+        if( curr->next != nullptr )
+            std::cout << " -> ";
+        curr = curr->next;
+        if( curr == original )
+        {
+            std::cout << "endless loop, breaking" << std::endl;
+            break;
+        }
     }
     std::cout << std::endl;
 }
 
 
-std::vector<int> testFunc( std::vector<int> &nums )
+
+ListNode *removeZeroSumSublists(ListNode *head)
 {
-    std::vector<int> retval = nums;
-    std::unordered_map<int,int> psums;
-    int running_total;
-    for(int index=0;index<nums.size();index++)
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    
+    int running_sum = 0;
+    std::unordered_map<int,ListNode *> prefixes;
+
+    ListNode *curr = dummy;
+    while( curr != nullptr )
     {
+        running_sum += curr->val;
+        prefixes[running_sum] = curr;
+        curr = curr->next;
     }
+
+    curr = dummy;
+    running_sum = 0;
+    while( curr != nullptr )
+    {
+        running_sum += curr->val;
+        curr->next = prefixes[running_sum]->next;
+        curr = curr->next;
+    }
+    
+    return dummy->next;
 }
+
 
 int main(int argc, char **argv)
 {
     std::cout << std::endl << "1171-remove-zero-sum-consecutive-nodes-from-linked-list" << std::endl << std::endl;
     int test_case = 1;
     {
-        std::vector<int> nums     = {1,2,-3,3,1};
-        std::vector<int> expected = {3,1};
-        std::vector<int> result   = testFunc( nums );
+        ListNode *one    = new ListNode(1);
+        ListNode *two    = new ListNode(2);
+        ListNode *min3   = new ListNode(-3);
+        ListNode *three  = new ListNode(3);
+        ListNode *onee   = new ListNode(1);
+
+        one->next = two;
+        two->next = min3;
+        min3->next = three;
+        three->next = onee;
+        ListNode *head = one;
+        dumpList(head);
+        ListNode *nhead = removeZeroSumSublists(head);
+        dumpList(nhead);
+
+        bool expected = true;
+        bool result = true;
         std::cout << std::endl;
         std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
         std::cout << std::endl;
