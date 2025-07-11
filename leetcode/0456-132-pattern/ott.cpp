838192584703d65cf9ac32d1c5cd8f3795564f6b
Alfonso Urdaneta <alfonso@red82.com>
2025-07-11 17:14:11 -0400

    progress checkin

---
+++ leetcode/0456-132-pattern/ott.cpp
@@ -0,0 +1,62 @@
+// -*- Mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; -*-
+//
+//  red82 // software
+//
+//  This software may not be used or reproduced, in whole or in part,
+//  without the express written permission of red82
+
+#include <iostream>
+#include <iomanip>
+#include <optional>
+#include <vector>
+#include <queue>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <limits.h>
+
+
+bool find132pattern(std::vector<int> &nums)
+{
+    std::vector<int> monos( -1, nums.size());
+    bool retval = false;
+    return retval;
+}
+
+int main(int argc, char **argv)
+{
+    std::cout << std::endl << "0456-132-pattern" << std::endl << std::endl;
+    int test_case = 1;
+    {
+        std::vector<int> nums  = {1,2,3,4};
+        bool expected = false;
+        bool result = find132pattern(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<int> nums  = {3,1,4,2};
+        bool expected = false;
+        bool result = find132pattern(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<int> nums  = {-1,3,2,0};
+        bool expected = false;
+        bool result = find132pattern(nums);
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
+        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    return 0;
+}
