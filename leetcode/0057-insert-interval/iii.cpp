3ee6ab53042fbc0e35235e6bb0f7d2824271b744
Alfonso Urdaneta <alfonso@red82.com>
2025-07-24 16:41:08 -0400

    progress checkin

--- leetcode/0057-insert-interval/iii.cpp
+++ leetcode/0057-insert-interval/iii.cpp
@@ -16,6 +16,11 @@
 #include <stack>
 #include <limits.h>
 
+void dumpPair(std::vector<int> pair)
+{
+    std::cout << "[" << pair[0] << "," << pair[1] << "]" ;
+}
+
 bool overlaps(std::vector<int> first, std::vector<int> second)
 {
     if( (second[0] >= first[0]) && ((second[0]) <= first[1]))
@@ -34,22 +39,48 @@ std::vector<int> merge(std::vector<int> first,
 {
     std::vector<int> retval;
     retval.push_back( std::min( first[0], second[0]));
-    retval.push_back( std::max( first[1],second[1]));
+    retval.push_back( std::max( first[1], second[1]));
     return retval;
 }
 
 std::vector<std::vector<int>> insert(std::vector<std::vector<int>>& intervals, 
                                      std::vector<int>& newInterval) 
 {
+    bool new_was_inserted = false;
     std::vector<std::vector<int>> retval;
-    for(auto curr : intervals )
+    int index = 0;
+    while( index < intervals.size() )
     {
+        std::vector<int> *insertion_candidate;
+        if(( new_was_inserted == false ) && ( newInterval[0] < intervals[index][0] ))
+        {
+            insertion_candidate = &newInterval;
+            new_was_inserted = true;
+        }
+        else
+        {
+            insertion_candidate = &(intervals[index]);
+            index++;
+        }
         if( retval.empty())
-            retval.push_back( curr );
-        
-        std::vector<int> &last = retval.back();
-        if( overlaps( ))
-    }        
+        {
+            retval.push_back( *insertion_candidate );
+        }
+        else
+        {
+            std::vector<int> &last = retval.back();
+            if( overlaps( last, *insertion_candidate ))
+            {
+                std::vector<int> merged = merge( last, *insertion_candidate );
+                retval.pop_back();
+                retval.push_back( merged );
+            }
+            else
+            {
+                retval.push_back( *insertion_candidate );
+            }
+        }
+    }
     return retval;
 }
 
@@ -58,12 +89,44 @@ int main(int argc, char **argv)
     std::cout << std::endl << "0057-insert-interval" << std::endl << std::endl;
     int test_case = 1;
     {
-        std::vector<int> nums  = {1,2,3};
-        int expected = 0;
+        std::vector<std::vector<int>> intervals = {{1,3},{6,9}};
+        std::vector<int> newInterval = { 2,5 };
+        std::vector<std::vector<int>> mints = insert( intervals, newInterval );
+        std::cout << "[" ;
+        bool first_time = true;
+        for(const auto &curr : mints )
+        {
+            if( ! first_time )
+                std::cout << std::endl;
+            first_time = false;
+            dumpPair(curr);
+        }
+        std::cout << "]" ;
         int result = 0;
+        int expected = 0;
         std::cout << std::endl;
         std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
-        std::cout << " (expected " << expected << ", got " << result << ")\n";
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    {
+        std::vector<std::vector<int>> intervals = {{1,2},{3,5},{6,7},{8,10},{12,16}};
+        std::vector<int> newInterval = { 4, 8 };
+        std::vector<std::vector<int>> mints = insert( intervals, newInterval );
+        std::cout << "[" ;
+        bool first_time = true;
+        for(const auto &curr : mints )
+        {
+            if( ! first_time )
+                std::cout << std::endl;
+            first_time = false;
+            dumpPair(curr);
+        }
+        std::cout << "]" ;
+        int result = 0;
+        int expected = 0;
+        std::cout << std::endl;
+        std::cout << "Test case : " << test_case++ << " : " << (expected == result ? "Pass" : "Fail")  << std::endl;
         std::cout << std::endl;
         std::cout << std::endl;
     }
