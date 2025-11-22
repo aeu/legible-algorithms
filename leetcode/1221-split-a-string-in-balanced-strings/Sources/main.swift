529f95afb2ff94bf73ce2396a571963a43cdbd35
Alfonso Urdaneta <alfonso@red82.com>
2025-11-22 20:48:18 +0000

    LC1221 - split a string in balanced strings

--- leetcode/1221-split-a-string-in-balanced-strings/Sources/main.swift
+++ leetcode/1221-split-a-string-in-balanced-strings/Sources/main.swift
@@ -7,21 +7,44 @@ class Solution
 {
     func balancedStringSplit(_ s: String) -> Int
     {
-        return 0;
+        var balance = 0
+        var total = 0
+        for current_char in s {
+            if current_char == "L" {
+                balance = balance + 1
+            }
+            else {
+                balance = balance - 1
+            }
+            if balance == 0 {
+                total = total + 1
+            }
+        }
+        return total
     }
 
     static func main()
     {
-        print("leetcode/1221-split-a-string-in-balanced-strings/")
+        print("##leetcode/1221-split-a-string-in-balanced-strings/")
+        let sol = Solution()
+        
         var test_case = 0
         var expected = 4
-        let sol = Solution()
         var result = sol.balancedStringSplit("RLRRLLRLRL")
+        print(String(format: "Test case %d : %@,", test_case, ((expected == result) ? "Pass" : "Fail")))
+
+
+        test_case += 1
+        expected = 2
+        result = sol.balancedStringSplit("RLRRRLLRLL")
+        print(String(format: "Test case %d : %@,", test_case, ((expected == result) ? "Pass" : "Fail")))
+
+        test_case += 1
+        expected = 1
+        result = sol.balancedStringSplit("LLLLRRRR")
         print(String(format: "Test case %d : %@,", test_case, ((expected == result) ? "Pass" : "Fail")))
     }
 }
 
 Solution.main()
 
-
-print("Hello, world!")
