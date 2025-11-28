d87b8b9bd1e28dab1bd87d108c2e1c2793c65d39
Alfonso Urdaneta <alfonso@red82.com>
2025-11-28 20:53:53 +0000

    LC223 - rectangle area (swift)

---
+++ leetcode/0223-rectangle-area/README.md
@@ -0,0 +1,11 @@
+### leetcode/0223-rectangle-area
+```
+Expected: 45
+Result: 45
+Test case 1: Pass
+```
+```
+Expected: 16
+Result: 16
+Test case 2: Pass
+```
---
+++ leetcode/0223-rectangle-area/Sources/main.swift
@@ -0,0 +1,63 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+class Solution
+{
+    func computeArea(_ ax1: Int, 
+                     _ ay1: Int, 
+                     _ ax2: Int, 
+                     _ ay2: Int, 
+                     _ bx1: Int, 
+                     _ by1: Int, 
+                     _ bx2: Int, 
+                     _ by2: Int) -> Int {
+        var computed_area = 0
+        let ileft  = max(ax1,bx1)
+        let ibot   = max(ay1,by1)
+        let iright = min(ax2,bx2)
+        let itop   = min(ay2,by2)
+        var iarea = 0;
+        if ( iright > ileft ) && ( itop > ibot ) {
+            let width = iright - ileft;
+            let height = itop - ibot;
+            iarea = width * height;
+        }
+
+        let aarea = (ax2-ax1)*(ay2-ay1);
+        let barea = (bx2-bx1)*(by2-by1);
+
+        return aarea + barea - iarea;
+    }
+
+
+    static func main()
+    {
+        print("### leetcode/0223-rectangle-area")
+        let sol = Solution()
+        var test_case = 1
+        var expected = 45
+        var result = sol.computeArea(-3,0,3,4,0,-1,9,2)
+        print("```")
+        print("Expected: \(expected)")
+        print("Result: \(result)")
+        print("Test case \(test_case): \(expected == result ? "Pass" : "Fail")")
+        print("```")
+
+
+        test_case += 1
+        expected = 16
+        result = sol.computeArea(-2,-2,2,2,-2,-2,2,2)
+        print("```")
+        print("Expected: \(expected)")
+        print("Result: \(result)")
+        print("Test case \(test_case): \(expected == result ? "Pass" : "Fail")")
+        print("```")
+        
+        
+    }
+}
+
+Solution.main()
+
