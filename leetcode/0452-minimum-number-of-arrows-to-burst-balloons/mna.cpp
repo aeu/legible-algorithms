eec9b798173e776a81b91eedddade2924ca3241c
Alfonso Urdaneta <alfonso@red82.com>
2026-04-13 19:43:44 -0400

    LC452 cleanup logic and comments

--- leetcode/0452-minimum-number-of-arrows-to-burst-balloons/mna.cpp
+++ leetcode/0452-minimum-number-of-arrows-to-burst-balloons/mna.cpp
@@ -13,10 +13,6 @@
 #include <stack>
 
 
-#if 0
-
-#endif
-
 void dumpBalloons(std::vector<std::vector<int>> &points)
 {
     bool first_time = true;
@@ -37,8 +33,11 @@ bool isPopped(std::vector<int> balloon, int y_position)
     return false;
 }
 
+
 int findMinArrowShots(std::vector<std::vector<int>>& points)
 {
+    // sort all the baloons, using the end of their span as the
+    // differentiator.
     std::sort(points.begin(),
               points.end(),
               [](std::vector<int> left, std::vector<int> right)
@@ -46,24 +45,31 @@ int findMinArrowShots(std::vector<std::vector<int>>& points)
                   return left[1] < right[1];
               });
     
-    int arrow_shots = 0;
-    int arrow_y = 0;
+    int shot_count = 0;
+    int arrow_position = 0;
     bool first_time = true;
+    // loop through all the balloons
     for(auto current_balloon : points )
     {
+        // for the first balloon, shoot at its tail end (this is the
+        // last possible point on the baloon and the most optimal choice to
+        // intersect with the subsequent balloon).  Note that we store
+        // the arrow shot to see if it hits the next balloon 
         if( first_time )
         {
-            arrow_y = current_balloon[1];
-            arrow_shots++;
+            arrow_position = current_balloon[1];
+            shot_count++;
             first_time = false;
         }
-        if( ! isPopped( current_balloon, arrow_y ))
+        if( ! isPopped( current_balloon, arrow_position ))
         {
-            arrow_y = current_balloon[1];
-            arrow_shots++;
+            // the current balloon wasn't popped by the last arrow, so
+            // we have to shoot another one.
+            arrow_position = current_balloon[1];
+            shot_count++;
         }
     }
-    return arrow_shots;
+    return shot_count;
 }
 
 
@@ -77,22 +83,22 @@ int main(int argc, char **argv)
         std::cout << std::endl << "Example 1" << std::endl;
         std::vector<std::vector<int>> points =  {{10,16},{2,8},{1,6},{7,12}};
         dumpBalloons( points );
-        int min_arrow_shots = findMinArrowShots(points);
-        std::cout << "Min arrow shots : " << min_arrow_shots << std::endl;
+        int min_shot_count = findMinArrowShots(points);
+        std::cout << "Min arrow shots : " << min_shot_count << std::endl;
     }
     {
         std::cout << std::endl << "Example 2" << std::endl;
         std::vector<std::vector<int>> points = {{1,2},{3,4},{5,6},{7,8}};
         dumpBalloons( points );
-        int min_arrow_shots = findMinArrowShots(points);
-        std::cout << "Min arrow shots : " << min_arrow_shots << std::endl;
+        int min_shot_count = findMinArrowShots(points);
+        std::cout << "Min arrow shots : " << min_shot_count << std::endl;
     }
     {
         std::cout << std::endl << "Example 3" << std::endl;
         std::vector<std::vector<int>> points =  {{1,2},{2,3},{3,4},{4,5}};
         dumpBalloons( points );
-        int min_arrow_shots = findMinArrowShots(points);
-        std::cout << "Min arrow shots : " << min_arrow_shots << std::endl;
+        int min_shot_count = findMinArrowShots(points);
+        std::cout << "Min arrow shots : " << min_shot_count << std::endl;
     }
     return -1;
 }
