17da8cabeb785d735f1b4a68a206b0eb9496fa11
Alfonso Urdaneta <alfonso@red82.com>
2026-04-30 17:03:20 -0400

    progress checkin

--- leetcode/1856-maximum-subarray-min-product/msm.cpp
+++ leetcode/1856-maximum-subarray-min-product/msm.cpp
@@ -16,41 +16,94 @@
 #include <unordered_map>
 #include <stack>
 #include <limits.h>
+#include <span>
 
 
 void dumpValues(std::vector<int> &values)
 {
+    std::cout << "[" ;
     bool first = true;
     for(const auto curr : values )
     {
         if( first )
             first = false;
         else
-            std::cout << " -> " ;
+            std::cout << ", " ;
         std::cout << curr ;
     }
-    std::cout << std::endl;
+    std::cout << "]" << std::endl;
+}
+
+void dumpSpan(std::span<int> &values)
+{
+    std::cout << "[" ;
+    bool first = true;
+    for(const auto curr : values )
+    {
+        if( first )
+            first = false;
+        else
+            std::cout << ", " ;
+        std::cout << curr ;
+    }
+    std::cout << "]" << std::endl;
 }
 
 int maxSumMinProduct(std::vector<int> &nums )
 {
     std::vector<int> lessers_left(nums.size(),-1);
-    std::vector<int> highers_right(nums.size(),-1);
+    std::vector<int> lessers_right(nums.size(),-1);
 
     dumpValues(nums);
     
     std::stack<int> monostack;
     for(int index=0;index<nums.size();index++)
     {
-        while(( ! monostack.empty() ) && ( nums[index] > nums[ monostack.top()] ))
-        {
-            lessers_left[index] = monostack.top();
+        while(( ! monostack.empty() ) && ( nums[ monostack.top()] >= nums[index] ))
             monostack.pop();
-        }
+
+        lessers_left[index] = monostack.empty() ? -1 : monostack.top();
         monostack.push(index);
     }
     dumpValues(lessers_left);
+
+    while( ! monostack.empty() )
+        monostack.pop();
+
+    for(int index=nums.size()-1;index>=0;index--)
+    {
+        while(( ! monostack.empty()) && ( nums[monostack.top()] >= nums[index]  ))
+            monostack.pop();
+        lessers_right[index] = monostack.empty() ? -1 : monostack.top();
+        monostack.push(index);
+    }
+    dumpValues(lessers_right);
+
+    int start;
+    int end;
+    for(int index=0;index<nums.size();index++)
+    {
+        std::cout << nums[index] << std::endl;
+        if( lessers_left[index] == -1 )
+            start = 0;
+        else
+            start = lessers_left[index] + 1;
+
+        // if( start > index )
+        //     start = index;
+
+        if( lessers_right[index] == -1 )
+            end = nums.size();
+        else
+            end = lessers_right[index];
+
+        std::span<int> span( nums.begin()+start, nums.begin()+end);
+        std::cout << "\tis the lowest in the subarray : " ;
+        dumpSpan(span);
+    }
+
     return 14;
+
 }
 
 int main(int argc, char **argv)
